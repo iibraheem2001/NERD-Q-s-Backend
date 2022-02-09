d632248f93e196af567d6dd18ef99face8b9931b
Cedric Vallieu <cvallieu94@gmail.com>
2022-02-08 20:55:08 -0800

    Updated backend server

--- server.js
+++ server.js
@@ -7,38 +7,48 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const mongoose = require('mongoose');
-const ex = require('./models/ex.js');
-const verifyUser = require('./authentication.js');
+// const mongoose = require('mongoose');
+// const ex = require('./models/ex.js');
+// const verifyUser = require('./authentication.js');
 const PORT = process.env.PORT || 3002;
 
-mongoose.connect(process.env.MONGODB_URO, { userNewUrlParser: true, useUnifiedTopology: true })
-    .then(console.log('Mongoose is connected'));
+// mongoose.connect(process.env.MONGODB_URO, { userNewUrlParser: true, useUnifiedTopology: true })
+//     .then(console.log('Mongoose is connected'));
 
 app.get('/', (request, response) => {
     response.send('test request recieved')
 })
 
 app.get('/videogames', handleGetVideogames);
-app.get('/cars', handleGetCars);
-app.get('/sports', handleGetSports);
+// app.get('/cars', handleGetCars);
+// app.get('/sports', handleGetSports);
+
+const TriviaModel = require('./models/ex.js');
+
+const Data = { };
 
 async function handleGetVideogames(request, response) {
-    try {
-        let queryObj = {};
-        if (request.query.email) {
-            queryObj = { email: request.query.email }
-        }
-        let exFromDB = await ex.find(queryObj);
+    // try {
+    //     let queryObj = {};
+    //     if (request.query.email) {
+    //         queryObj = { email: request.query.email }
+    //     }
+    //     let exFromDB = await ex.find(queryObj);
 
-        if (exFromDB) {
-            response.status(200).send(exFromDB);
-        } else {
-            response.status(404).send('trivia not found.');
-        }
+    //     if (exFromDB) {
+    //         response.status(200).send(exFromDB);
+    //     } else {
+    //         response.status(404).send('trivia not found.');
+    //     }
+
+    try {
+        const data = await axios.get('https://opentdb.com/api.php?amount=10&category=15&difficulty=easy');
+        const item = new TriviaModel(data);
+        await item.save();
+        res.status(200).json(item);
     } catch (error) {
         console.error(error);
-        response.status(500).send('Server error.');
+        response.status(500).send('trivia not found.');
     }
 }
 
